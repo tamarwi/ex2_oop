987d1377cd6bbc4a325bd4e47910d5769ad5ff43
רואי נתנזון <T9079575@talpiot.huji.ac.il>
2024-02-05 14:57:25 +0200

    added consts

---
+++ src/bricker/Constants.java
@@ -0,0 +1,11 @@
+package bricker;
+
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+
+public class Constants {
+    public static String MAIN_PADDLE_TAG = "MainPaddle";
+    public static String SECONDARY_PADDLE_TAG = "SecondaryPaddle";
+    public static Vector2 HEART_SPEED = new Vector2(0, 100);
+    public static Vector2 HEART_DIMENSIONS = new Vector2( 20,20);
+}
--- src/bricker/gameobjects/Heart.java
+++ src/bricker/gameobjects/Heart.java
@@ -1,4 +1,32 @@
 package bricker.gameobjects;
 
-public class Heart {
+import bricker.Constants;
+import danogl.GameObject;
+import danogl.collisions.Collision;
+import danogl.gui.Sound;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+
+public class Heart extends GameObject {
+    private Sound collisionSound;
+
+    public Heart(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable,
+                Sound collisionSound) {
+
+        super(topLeftCorner, Constants.HEART_DIMENSIONS, renderable);
+        this.setVelocity(Constants.HEART_SPEED);
+        this.collisionSound = collisionSound;
+    }
+
+    @Override
+    public void onCollisionEnter(GameObject other, Collision collision) {
+        super.onCollisionEnter(other, collision);
+        this.collisionSound.play();
+        // ADD LIFE
+    }
+
+    @Override
+    public boolean shouldCollideWith(GameObject other){
+        return other.getTag().equals(Constants.MAIN_PADDLE_TAG);
+    }
 }
