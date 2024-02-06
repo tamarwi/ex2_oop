f8b6cd3effbd3de5ce7c4cfed67fd58eaf03f468
tamarwi <widmann.tamar@gmail.com>
2024-02-06 21:45:19 +0200

    added docu

--- src/bricker/Constants.java
+++ src/bricker/Constants.java
@@ -3,28 +3,122 @@ package bricker;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
+/**
+ * Constants class contains constant values used throughout the game.
+ */
 public class Constants {
+    /**
+     * Tag for the main paddle object.
+     */
     public static final String MAIN_PADDLE_TAG = "MainPaddle";
+
+    /**
+     * Tag for wall objects.
+     */
     public static final String WALL_TAG = "Wall";
+
+    /**
+     * Tag for the main ball object.
+     */
     public static final String MAIN_BALL_TAG = "MainBall";
+
+    /**
+     * Speed vector for the heart power-up.
+     */
     public static final Vector2 HEART_SPEED = new Vector2(0, 100);
-    public static final Vector2 HEART_DIMENSIONS = new Vector2( 20,20);
+
+    /**
+     * Dimensions of the heart power-up.
+     */
+    public static final Vector2 HEART_DIMENSIONS = new Vector2(20, 20);
+
+    /**
+     * Speed of the ball object.
+     */
     public static final float BALL_SPEED = 100;
-    public static final int PADDLE_WIDTH =100;
+
+    /**
+     * Width of the paddle object.
+     */
+    public static final int PADDLE_WIDTH = 100;
+
+    /**
+     * Height of the paddle object.
+     */
     public static final int PADDLE_HEIGHT = 15;
+
+    /**
+     * Radius of the ball object.
+     */
     public static final int BALL_RADIUS = 20;
+
+    /**
+     * File path for the ball image.
+     */
     public static final String BALL_IMAGE_PATH = "assets/ball.png";
+
+    /**
+     * File path for the puck image.
+     */
     public static final String PUCK_IMAGE_PATH = "assets/mockBall.png";
+
+    /**
+     * File path for the paddle image.
+     */
     public static final String PADDLE_IMAGE_PATH = "assets/paddle.png";
+
+    /**
+     * File path for the heart image.
+     */
     public static final String HEART_IMAGE_PATH = "assets/heart.png";
+
+    /**
+     * File path for the brick image.
+     */
     public static final String BRICK_IMAGE_PATH = "assets/brick.png";
+
+    /**
+     * File path for the background image.
+     */
     public static final String BACKGROUND_IMAGE_PATH = "assets/DARK_BG2_small.jpeg";
+
+    /**
+     * File path for the collision sound.
+     */
     public static final String COLLISION_SOUND_PATH = "assets/blop_cut_silenced.wav";
+
+    /**
+     * Default number of brick rows in the game.
+     */
     public static final int DEFAULT_NUMBER_BRICK_ROWS = 7;
+
+    /**
+     * Default number of bricks in each row.
+     */
     public static final int DEFAULT_NUMBER_BRICKS_IN_ROW = 8;
+
+    /**
+     * Number of lives a player starts with.
+     */
     public static final int NUMBER_OF_LIVES = 3;
+
+    /**
+     * Maximum number of lives a player can have.
+     */
     public static final int MAX_NUMBER_OF_LIVES = 4;
+
+    /**
+     * Maximum number of collision strategies per brick.
+     */
     public static final int MAX_STRATEGIES_PER_BRICK = 3;
+
+    /**
+     * Number of hits required for the temporal paddle power-up.
+     */
     public static final int TEMPORAL_PADDLE_NUM_HITS = 4;
+
+    /**
+     * Number of collisions before resetting the camera focus on the ball.
+     */
     public static final int NUM_COLLISIONS_BEFORE_CAMERA_RESET = 4;
 }
--- src/bricker/gameobjects/Brick.java
+++ src/bricker/gameobjects/Brick.java
@@ -7,35 +7,48 @@ import danogl.collisions.Collision;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
+/**
+ * Brick class represents a brick game object.
+ * Extends GameObject.
+ */
 public class Brick extends GameObject {
-    private static final int BRICK_HEIGHT  = 15;
+    private static final int BRICK_HEIGHT = 15;
     private CollisionStrategy collisionStrategy;
 
     /**
-     * Construct a new GameObject instance.
-     *
-     * @param topLeftCorner Position of the object, in window coordinates (pixels).
+     * Constructor for Brick.
+     * @param topLeftCorner The position of the object, in window coordinates (pixels).
      *                      Note that (0,0) is the top-left corner of the window.
-     * @param dimensions    Width and height in window coordinates.
-     * @param renderable    The renderable representing the object. Can be null, in which case
-     *                      the GameObject will not be rendered.
+     * @param dimensions The width and height in window coordinates.
+     * @param renderable The renderable representing the object. Can be null, in which case
+     *                   the GameObject will not be rendered.
+     * @param collisionStrategy The CollisionStrategy object to handle collisions with the brick.
      */
     public Brick(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable,
                  CollisionStrategy collisionStrategy) {
         super(topLeftCorner, dimensions, renderable);
         this.collisionStrategy = collisionStrategy;
-
     }
 
+    /**
+     * Handles the event when a collision occurs with another game object.
+     * @param other The other game object involved in the collision.
+     * @param collision The Collision object representing the collision information.
+     */
     @Override
     public void onCollisionEnter(GameObject other, Collision collision) {
-            super.onCollisionEnter(other, collision);
-            this.collisionStrategy.onCollision(this, other);
+        super.onCollisionEnter(other, collision);
+        this.collisionStrategy.onCollision(this, other);
     }
 
+    /**
+     * Determines if the brick should collide with another game object.
+     * Bricks should collide with all objects except walls.
+     * @param other The other game object.
+     * @return True if the brick should collide with the other game object, false otherwise.
+     */
     @Override
-    public boolean shouldCollideWith(GameObject other){
+    public boolean shouldCollideWith(GameObject other) {
         return !other.getTag().equals(Constants.WALL_TAG);
     }
-
 }
