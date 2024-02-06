71fc24486355c8dc9acd33d30a6f83ed045c2143
tamarwi <widmann.tamar@gmail.com>
2024-02-06 21:38:11 +0200

    main docu

--- src/bricker/main/BrickerGameManager.java
+++ src/bricker/main/BrickerGameManager.java
@@ -16,6 +16,10 @@ import java.awt.event.InputMethodListener;
 import java.awt.event.KeyEvent;
 import java.util.Random;
 
+/**
+ * BrickerGameManager manages the game logic for the Bricker game.
+ * Extends GameManager.
+ */
 public class BrickerGameManager extends GameManager {
     private int number_brick_rows;
     private int number_bricks_in_row;
@@ -28,10 +32,23 @@ public class BrickerGameManager extends GameManager {
     private TemporalPaddle temporalPaddle;
     private int lastBallNumCollisions;
 
+    /**
+     * Constructor
+     * * @param windowTitle The title of the game window.
+     * * @param windowDimensions The dimensions of the game window.
+     */
     public BrickerGameManager(String windowTitle, Vector2 windowDimensions) {
         super(windowTitle, windowDimensions);
     }
 
+    /**
+     * Initializes the game resources and objects.
+     *
+     * @param imageReader      The ImageReader object to read image resources.
+     * @param soundReader      The SoundReader object to read sound resources.
+     * @param inputListener    The UserInputListener object to handle user input.
+     * @param windowController The WindowController object to control the game window.
+     */
     @Override
     public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
@@ -61,6 +78,11 @@ public class BrickerGameManager extends GameManager {
         createWalls();
     }
 
+    /**
+     * Updates the game logic.
+     *
+     * @param deltaTime The time elapsed since the last update.
+     */
     @Override
     public void update(float deltaTime) {
         super.update(deltaTime);
@@ -68,10 +90,18 @@ public class BrickerGameManager extends GameManager {
         checkForCameraReset();
     }
 
-    public Vector2 getWindowDimensions(){
+    /**
+     * Gets the dimensions of the game window.
+     *
+     * @return The Vector2 object representing the window dimensions.
+     */
+    public Vector2 getWindowDimensions() {
         return this.windowDimensions;
     }
 
+    /**
+     * Checks for the end of the game.
+     */
     private void checkForGameEnd() {
         float ballHeight = ball.getCenter().y();
         String prompt = "";
@@ -84,10 +114,10 @@ public class BrickerGameManager extends GameManager {
                 resetBall();
             }
         }
-        if(this.numberOfBricks == 0){
+        if (this.numberOfBricks == 0) {
             prompt = "you win! play again?";
         }
-        if(inputListener.isKeyPressed(KeyEvent.VK_W)) {
+        if (inputListener.isKeyPressed(KeyEvent.VK_W)) {
             prompt = "You win! Play again?";
         }
 
@@ -100,6 +130,9 @@ public class BrickerGameManager extends GameManager {
         }
     }
 
+    /**
+     * Resets the ball's position and velocity.
+     */
     private void resetBall() {
         this.ball.setCenter(windowDimensions.mult(0.5F));
 
@@ -115,6 +148,12 @@ public class BrickerGameManager extends GameManager {
         ball.setVelocity(new Vector2(ballVelX, ballVelY));
     }
 
+    /**
+     * Creates the ball game object.
+     *
+     * @param imageReader The ImageReader object to read ball image.
+     * @param soundReader The SoundReader object to read collision sound.
+     */
     private void createBall(ImageReader imageReader, SoundReader soundReader) {
         Ball ball = new Ball(new Vector2(0, 0), new Vector2(Constants.BALL_RADIUS,
                 Constants.BALL_RADIUS),
@@ -126,15 +165,28 @@ public class BrickerGameManager extends GameManager {
 
     }
 
+    /**
+     * Adds a game object to the game.
+     *
+     * @param gameObject The game object to add.
+     */
     public void addGameObject(GameObject gameObject) {
         gameObjects().addGameObject(gameObject);
     }
 
+    /**
+     * Removes a game object from the game.
+     *
+     * @param gameObject The game object to remove.
+     */
     public void removeGameObject(GameObject gameObject) {
         gameObjects().removeGameObject(gameObject);
     }
 
-    public void focusCameraOnBall(){
+    /**
+     * Focuses the camera on the ball.
+     */
+    public void focusCameraOnBall() {
         lastBallNumCollisions = ball.getCollisionCounter();
         this.setCamera(
                 new Camera(
@@ -146,13 +198,22 @@ public class BrickerGameManager extends GameManager {
         );
     }
 
-    private void checkForCameraReset(){
-        if(ball.getCollisionCounter() >= lastBallNumCollisions +
-                Constants.NUM_COLLISIONS_BEFORE_CAMERA_RESET){
+    /**
+     * Checks for camera reset condition.
+     */
+    private void checkForCameraReset() {
+        if (ball.getCollisionCounter() >= lastBallNumCollisions +
+                Constants.NUM_COLLISIONS_BEFORE_CAMERA_RESET) {
             setCamera(null);
         }
     }
 
+    /**
+     * Creates the user paddle game object.
+     *
+     * @param imageReader   The ImageReader object to read paddle image.
+     * @param inputListener The UserInputListener object to handle user input.
+     */
     private void createPaddle(ImageReader imageReader, UserInputListener inputListener) {
         GameObject userPaddle = new UserPaddle(new Vector2(0, 0), new Vector2(Constants.PADDLE_WIDTH,
                 Constants.PADDLE_HEIGHT),
@@ -162,8 +223,11 @@ public class BrickerGameManager extends GameManager {
         gameObjects().addGameObject(userPaddle);
     }
 
+    /**
+     * Creates a temporal paddle game object.
+     */
     public void createTemporalPaddle() {
-        if(this.temporalPaddle!=null){
+        if (this.temporalPaddle != null) {
             return;
         }
         TemporalPaddle temporalPaddle = new TemporalPaddle(new Vector2(0, 0), new Vector2(Constants.PADDLE_WIDTH,
@@ -174,33 +238,55 @@ public class BrickerGameManager extends GameManager {
         this.temporalPaddle = temporalPaddle;
     }
 
-    public void removeTemporalPaddle(){
+    /**
+     * Removes the temporal paddle game object.
+     */
+    public void removeTemporalPaddle() {
         this.temporalPaddle = null;
     }
 
+    /**
+     * Creates the lives game object.
+     *
+     * @param imageReader   The ImageReader object to read heart image.
+     * @param inputListener The UserInputListener object to handle user input.
+     */
     private void createLives(ImageReader imageReader, UserInputListener inputListener) {
         Lives lives = new Lives(new Vector2(20, 150), Constants.HEART_DIMENSIONS, //TODO 20,150 random heart placement.
                 Resources.heartImage);
         gameObjects().addGameObject(lives, Layer.UI);
         GameObject[] hearts = lives.getHearts();
-        for (int i=0; i<lives.getNumberOfLivesLeft(); ++i) {
+        for (int i = 0; i < lives.getNumberOfLivesLeft(); ++i) {
             gameObjects().addGameObject(hearts[i], Layer.UI);
         }
         this.lives = lives;
     }
 
-    public void addLife(){
+    /**
+     * Adds a life to the player's lives.
+     */
+    public void addLife() {
         this.lives.increaseLife();
         int num_lives = this.lives.getNumberOfLivesLeft();
-        gameObjects().addGameObject(lives.getHearts()[num_lives-1], Layer.UI);
+        gameObjects().addGameObject(lives.getHearts()[num_lives - 1], Layer.UI);
     }
 
-    private void createBackground(){
+    /**
+     * Creates the background game object.
+     */
+    private void createBackground() {
         GameObject background = new GameObject(Vector2.ZERO, windowDimensions, Resources.backgroundImage);
         gameObjects().addGameObject(background, Layer.BACKGROUND);
     }
 
-
+    /**
+     * Creates a brick game object.
+     *
+     * @param imageReader      The ImageReader object to read brick image.
+     * @param windowDimensions The dimensions of the game window.
+     * @param center           The center position of the brick.
+     * @param length           The length of the brick.
+     */
     private void createBrick(ImageReader imageReader, Vector2 windowDimensions, Vector2 center, int length) {
         CollisionStrategy strategy = CollisionStrategyBuilder.BuildCollisionStrategy(this);
         GameObject brick = new Brick(Vector2.ZERO, new Vector2(length, 15),
@@ -209,7 +295,10 @@ public class BrickerGameManager extends GameManager {
         gameObjects().addGameObject(brick);
     }
 
-    private void createWalls(){
+    /**
+     * Creates the wall game objects.
+     */
+    private void createWalls() {
         Vector2 sideWallDimensions = new Vector2(5, this.windowDimensions.y());
         GameObject leftWall = new GameObject(Vector2.ZERO, sideWallDimensions, null);
         GameObject rightWall = new GameObject(new Vector2(this.windowDimensions.x() - 5, 0), sideWallDimensions, null);
@@ -222,7 +311,11 @@ public class BrickerGameManager extends GameManager {
         gameObjects().addGameObject(topWall);
     }
 
-    private void createBricks(ImageReader imageReader){
+    /**
+     * Creates the bricks for the game.
+     * @param imageReader The ImageReader object to read brick image.
+     */
+    private void createBricks(ImageReader imageReader) {
         for (int i = 0; i < this.number_brick_rows; i++) {
             for (int j = 0; j < this.number_bricks_in_row; j++) {
                 createBrick(imageReader,
@@ -234,14 +327,26 @@ public class BrickerGameManager extends GameManager {
         this.numberOfBricks = this.number_bricks_in_row * this.number_brick_rows;
     }
 
-    public void setNumberOfBricks(int numberOfBricks){
+    /**
+     * Sets the number of bricks in the game.
+     * @param numberOfBricks The number of bricks to set.
+     */
+    public void setNumberOfBricks(int numberOfBricks) {
         this.numberOfBricks = numberOfBricks;
     }
 
-    public int getNumberOfBricks(){
+    /**
+     * Gets the number of bricks in the game.
+     * @return The number of bricks.
+     */
+    public int getNumberOfBricks() {
         return this.numberOfBricks;
     }
 
+    /**
+     * Parses command line arguments to get the number of brick rows and bricks in each row.
+     * @param args The command line arguments.
+     */
     public void getBrickRowsInfo(String[] args) {
         if (args.length == 0) {
             this.number_brick_rows = Constants.DEFAULT_NUMBER_BRICK_ROWS;
@@ -252,6 +357,10 @@ public class BrickerGameManager extends GameManager {
         }
     }
 
+    /**
+     * Entry point of the application.
+     * @param args The command line arguments.
+     */
     public static void main(String[] args) {
         BrickerGameManager brickerGameManager = new BrickerGameManager("Bouncing ball",
                 new Vector2(700, 500));
