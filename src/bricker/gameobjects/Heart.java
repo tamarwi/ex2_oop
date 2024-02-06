f62292f3d434c1c79764c78dc463b5fa24d146bc
tamarwi <widmann.tamar@gmail.com>
2024-02-06 21:41:10 +0200

    added docu

--- src/bricker/gameobjects/Heart.java
+++ src/bricker/gameobjects/Heart.java
@@ -9,14 +9,29 @@ import danogl.gui.Sound;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
+/**
+ * Heart class represents a heart game object.
+ * Extends GameObject.
+ */
 public class Heart extends GameObject {
     private BrickerGameManager gameManager;
+
+    /**
+     * Constructor for Heart.
+     * @param topLeftCorner The top left corner position of the heart.
+     * @param gameManager The BrickerGameManager instance managing the game.
+     */
     public Heart(Vector2 topLeftCorner, BrickerGameManager gameManager){
         super(topLeftCorner, Constants.HEART_DIMENSIONS, Resources.heartImage);
         this.setVelocity(Constants.HEART_SPEED);
         this.gameManager = gameManager;
     }
 
+    /**
+     * Handles the event when a collision occurs with another game object.
+     * @param other The other game object involved in the collision.
+     * @param collision The Collision object representing the collision information.
+     */
     @Override
     public void onCollisionEnter(GameObject other, Collision collision) {
         super.onCollisionEnter(other, collision);
@@ -24,6 +39,12 @@ public class Heart extends GameObject {
         this.gameManager.removeGameObject(this);
     }
 
+    /**
+     * Determines if the heart should collide with another game object.
+     * Hearts should only collide with the main paddle.
+     * @param other The other game object.
+     * @return True if the heart should collide with the other game object, false otherwise.
+     */
     @Override
     public boolean shouldCollideWith(GameObject other){
         return other.getTag().equals(Constants.MAIN_PADDLE_TAG);
--- src/bricker/gameobjects/Lives.java
+++ src/bricker/gameobjects/Lives.java
@@ -3,56 +3,70 @@ package bricker.gameobjects;
 import bricker.Constants;
 import bricker.Resources;
 import danogl.GameObject;
-import danogl.gui.Sound;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
-import java.util.Arrays;
-
-public class Lives extends GameObject{
+/**
+ * Lives class represents the player's lives in the game.
+ * Extends GameObject.
+ */
+public class Lives extends GameObject {
     private GameObject[] hearts;
     private int numberOfLivesLeft;
 
     /**
-     * Construct a new GameObject instance.
-     *
-     * @param topLeftCorner Position of the object, in window coordinates (pixels).
+     * Constructor for Lives.
+     * @param topLeftCorner The position of the object, in window coordinates (pixels).
      *                      Note that (0,0) is the top-left corner of the window.
-     * @param dimensions    Width and height in window coordinates.
-     * @param renderable    The renderable representing the object. Can be null, in which case
-     *                      the GameObject will not be rendered.
+     * @param dimensions The width and height in window coordinates.
+     * @param renderable The Renderable representing the object. Can be null, in which case
+     *                   the GameObject will not be rendered.
      */
     public Lives(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable) {
         super(topLeftCorner, dimensions, renderable);
 
         this.numberOfLivesLeft = Constants.NUMBER_OF_LIVES;
         this.hearts = new GameObject[Constants.MAX_NUMBER_OF_LIVES];
-        Vector2 heartDiffVector = new Vector2( 25, 0);
-        for(int i = 0; i < Constants.MAX_NUMBER_OF_LIVES; i++){
+        Vector2 heartDiffVector = new Vector2(25, 0);
+        for (int i = 0; i < Constants.MAX_NUMBER_OF_LIVES; i++) {
             this.hearts[i] = new Image(topLeftCorner.add(heartDiffVector.mult(i)), Resources.heartImage);
         }
     }
 
-    public GameObject[] getHearts(){
+    /**
+     * Gets the array of heart GameObjects.
+     * @return The array of heart GameObjects.
+     */
+    public GameObject[] getHearts() {
         return this.hearts;
     }
 
-    public int getNumberOfLivesLeft(){
+    /**
+     * Gets the number of lives left.
+     * @return The number of lives left.
+     */
+    public int getNumberOfLivesLeft() {
         return this.numberOfLivesLeft;
     }
 
-    public GameObject decreaseLife(){
-        if((this.numberOfLivesLeft--) == 1){
-            return null; //end of game
+    /**
+     * Decreases the player's life count and returns the heart GameObject to remove.
+     * @return The heart GameObject to remove, or null if the game ends.
+     */
+    public GameObject decreaseLife() {
+        if ((this.numberOfLivesLeft--) == 1) {
+            return null; // End of game
         }
 
-        return this.hearts[this.numberOfLivesLeft]; //more lives left
+        return this.hearts[this.numberOfLivesLeft]; // More lives left
     }
 
-    public void increaseLife(){
-        if(this.numberOfLivesLeft < Constants.MAX_NUMBER_OF_LIVES){
+    /**
+     * Increases the player's life count if it's less than the maximum number of lives.
+     */
+    public void increaseLife() {
+        if (this.numberOfLivesLeft < Constants.MAX_NUMBER_OF_LIVES) {
             this.numberOfLivesLeft++;
         }
     }
-
 }
