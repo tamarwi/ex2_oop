d3b41f8dc1bb2ebb3dd4780165e375cd6a014bf7
רואי נתנזון <T9079575@talpiot.huji.ac.il>
2024-02-11 09:29:03 +0200

    roei

--- src/bricker/main/BrickerGameManager.java
+++ src/bricker/main/BrickerGameManager.java
@@ -345,7 +345,7 @@ public class BrickerGameManager extends GameManager {
     private void createBrick(Vector2 topLeftCorner, int length) {
         CollisionStrategy strategy = CollisionStrategyBuilder.BuildCollisionStrategy(this);
         GameObject brick = new Brick(topLeftCorner, new Vector2(length, Constants.BRICK_HEIGHT),
-                Resources.brickImage, new DoubleStrategy(this));
+                Resources.brickImage, strategy);
         gameObjects().addGameObject(brick);
     }
 
